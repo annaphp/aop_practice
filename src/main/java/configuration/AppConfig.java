21023d0d9b50f2f023e7e68cfacbca09430b8523
Anna Ilchenko <mymihtav@gmail.com>
2017-02-19 16:45:19 -0800

    Spring AOP demo - before and after advice added

---
+++ src/main/java/configuration/AppConfig.java
@@ -0,0 +1,47 @@
+package configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import beans.*;
+
+@Configuration
+@EnableAspectJAutoProxy 
+public class AppConfig {
+	
+	@Bean
+	public Game mgame(){
+		//dependency injection through constructor
+		Game game = new BaseballGame(home());
+		//dependency injection through setter
+		game.setAwayTeam(away());
+		return game;
+	}
+	
+	@Bean
+	public Team home(){
+		return new RedSox();
+	}
+	
+	@Bean Team away(){
+		return new Cubs();
+	}
+
+	@Bean
+	//aspect bean
+	public Audience audience(){
+		return new Audience();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
