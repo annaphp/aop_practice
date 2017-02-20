604497115c43020089d0c6c30461e9215e8b411e
Anna Ilchenko <mymihtav@gmail.com>
2017-02-19 16:55:26 -0800

    moved aspect into own package

---
+++ src/main/java/aspects/Audience.java
@@ -0,0 +1,24 @@
+package aspects;
+
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class Audience {
+
+	@Pointcut("execution(** beans.BaseballGame.playGame(..))")
+	public void playGame(){}
+	
+	@Before("playGame()")
+	public void takeSeats(){
+		System.out.println("Taking seats and putting hats on.");
+	}
+	
+	@AfterReturning("playGame()")
+	public void applause() {
+		System.out.println("Hooray!!!");
+	}
+	
+}
--- src/main/java/configuration/AppConfig.java
+++ src/main/java/configuration/AppConfig.java
@@ -4,10 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+import aspects.Audience;
 import beans.*;
 
 @Configuration
-@EnableAspectJAutoProxy 
+@EnableAspectJAutoProxy  //enable aspects
 public class AppConfig {
 	
 	@Bean
